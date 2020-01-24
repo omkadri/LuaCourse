159163d86fe61f54a048f8eb9d0b0829d096db45
omkadri <omkadri@hotmail.com>
2020-01-23 19:30:17 -0600

    3. Set up if/else conditions

--- FirstGame/main.lua
+++ FirstGame/main.lua
@@ -1,6 +1,16 @@
-numberX = 5
-numberY = 10
+message = 0
+condition = -100
+
+if condition > 0 then
+  message = 1
+elseif condition < 0 then
+  message = -1
+else
+  message = "no conditions met"
+end
+
+
 function love.draw()
   love.graphics.setFont(love.graphics.newFont(50))
-  love.graphics.print(numberX + numberY)
+  love.graphics.print(message)
 end
