53a0b941e7f966511d7f4d4f6bba3935dc926c11
omkadri <omkadri@hotmail.com>
2020-01-23 19:19:57 -0600

    2. Created first variables

--- FirstGame/main.lua
+++ FirstGame/main.lua
@@ -1,4 +1,6 @@
+numberX = 5
+numberY = 10
 function love.draw()
   love.graphics.setFont(love.graphics.newFont(50))
-  love.graphics.print("Hello World")
+  love.graphics.print(numberX + numberY)
 end
